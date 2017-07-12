a2cf26f711a95f6e96e5c413c89bfbb17ac5983d
david krueger <dk12181@gmail.com>
2017-07-12 16:00:39 -0500

    posted sql syntax on petshopdatabase.sql

---
+++ petshopdatabase.sql
@@ -0,0 +1,21 @@
+CREATE TABLE "owners" (
+	"id" serial primary key,
+	"first_name" varchar (100) not null,
+	"last_name" varchar(100) not null
+);
+
+CREATE TABLE "pets" (
+	"id" serial primary key,
+	"name" varchar (100) not null,
+	"breed" varchar(100) not null,
+	"color" varchar (100) not null,
+	"owner_id" varchar(100) not null
+);
+
+
+CREATE TABLE "visits" (
+	"id" serial primary key,
+	"check_in_date" timestamp,
+	"check_out_date" timestamp,
+  "pet_id" varchar (100) not null
+);
